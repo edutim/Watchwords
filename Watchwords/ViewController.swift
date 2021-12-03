7ad950355de1c733c61b79dbd35a25ca5c7876a9
Tim Hart <tim@9red7.com>
2021-12-03 13:09:25 -0500

    Initial Commit

---
+++ Watchwords/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Watchwords
+//
+//  Created by Timothy Hart on 12/3/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
